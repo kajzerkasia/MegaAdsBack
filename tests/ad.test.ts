f3710e6a07dca8dc40ad89869f79136151471479
Kasia <kajzerkatarzyna1@gmail.com>
2023-02-13 15:17:43 +0100

    Added searching and listing of ads.

--- tests/ad.test.ts
+++ tests/ad.test.ts
@@ -1,6 +1,12 @@
 import {AdRecord} from "../records/ad.record";
+import {pool} from "../utils/db";
+import {AdEntity} from "../types";
 
-test('AdRecord return data from database for one entry.', async () => {
+afterAll(async () => {
+    await pool.end();
+});
+
+test('AdRecord.getOne return data from database for one entry.', async () => {
 
     const ad = await AdRecord.getOne('abc');
 
@@ -12,10 +18,54 @@ test('AdRecord return data from database for one entry.', async () => {
 
 });
 
-test('AdRecord return null from database for unexisting entry.', async () => {
+test('AdRecord.getOne return null from database for unexisting entry.', async () => {
 
     const ad = await AdRecord.getOne('---');
 
     expect(ad).toBeNull();
+
+});
+
+test('AdRecord.findAll return array of found entries.', async () => {
+
+    const ads = await AdRecord.findAll('');
+
+    expect(ads).not.toEqual([]);
+    expect(ads[0].id).toBeDefined();
+
+});
+
+test('AdRecord.findAll return array of found entries when searching for "a".', async () => {
+
+    const ads = await AdRecord.findAll('a');
+
+    expect(ads).not.toEqual([]);
+    expect(ads[0].id).toBeDefined();
+
+});
+
+test('AdRecord.findAll return array of found entries when searching for "a".', async () => {
+
+    const ads = await AdRecord.findAll('a');
+
+    expect(ads).not.toEqual([]);
+    expect(ads[0].id).toBeDefined();
+
+});
+
+test('AdRecord.findAll return empty array when searching for something that does not exists.', async () => {
+
+    const ads = await AdRecord.findAll('---------------------------------------');
+
+    expect(ads).toEqual([]);
+
+});
+
+test('AdRecord.findAll returns smaller amount of data.', async () => {
+
+    const ads = await AdRecord.findAll('');
+
+    expect((ads[0] as AdEntity).price).toBeUndefined();
+    expect((ads[0] as AdEntity).description).toBeUndefined();
 
 });
